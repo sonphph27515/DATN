8898b0f516052eb1ff629c070fb0ef6f6d804437
sonphph27515@fpt.edu.vn <sonphph27515@fpt.edu.vn>
2024-12-13 22:12:47 +0700

    update

--- Website-Ban-Quan-Ao/src/main/java/com/example/demo/controllers/testController.java
+++ Website-Ban-Quan-Ao/src/main/java/com/example/demo/controllers/testController.java
@@ -1,8 +1,7 @@
-package com.example.demo.controller;
+package com.example.demo.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
