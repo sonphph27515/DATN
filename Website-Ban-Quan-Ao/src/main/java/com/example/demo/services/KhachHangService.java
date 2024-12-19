0c8403fb281351075ee941ba5bca27e0bf46314d
sonphph27515@fpt.edu.vn <sonphph27515@fpt.edu.vn>
2024-12-19 21:08:19 +0700

    update khach hang

---
+++ Website-Ban-Quan-Ao/src/main/java/com/example/demo/services/KhachHangService.java
@@ -0,0 +1,98 @@
+package com.example.demo.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KhachHangService {
+    @Autowired
+    private KhachHangRepository khachHangRepository;
+
+
+    //admin
+    public Page<KhachHangResponse> getPage(int page, int pageSize) {
+        Pageable pageable = PageRequest.of(page - 1, pageSize);
+        return khachHangRepository.getPage(pageable);
+    }
+
+    public List<KhachHangResponse> getAll() {
+        return khachHangRepository.findAllKhachHang();
+    }
+
+    public void add(KhachHangRequest khachHangRequest) {
+        KhachHang khachHang = new KhachHang();
+        khachHang.setHoVaTen(khachHangRequest.getHoVaTen());
+        khachHang.setEmail(khachHangRequest.getEmail());
+        khachHang.setSoDienThoai(khachHangRequest.getSoDienThoai());
+        khachHang.setMatKhau(khachHangRequest.getMatKhau());
+        khachHang.setDiaChi(khachHangRequest.getDiaChi());
+        khachHang.setXaPhuong(khachHangRequest.getXaPhuong());
+        khachHang.setQuanHuyen(khachHangRequest.getQuanHuyen());
+        khachHang.setTinhThanhPho(khachHangRequest.getTinhThanhPho());
+        khachHang.setTrangThai(0);
+        java.util.Date currentDate = new java.util.Date();
+        khachHang.setNgayTao(new java.sql.Date(currentDate.getTime()));
+        khachHang.setNgaySua(new java.sql.Date(currentDate.getTime()));
+
+        khachHangRepository.save(khachHang);
+
+        System.out.println("KhachHangService.add: " + khachHang.getHoVaTen());
+    }
+
+
+    public void update(KhachHangRequest khachHangRequest, UUID id) {
+        KhachHang khachHang = khachHangRepository.findById(id).orElse(null);
+        if (khachHang != null) {
+            khachHang.setHoVaTen(khachHangRequest.getHoVaTen());
+            khachHang.setEmail(khachHangRequest.getEmail());
+            khachHang.setSoDienThoai(khachHangRequest.getSoDienThoai());
+            khachHang.setDiaChi(khachHangRequest.getDiaChi());
+            khachHang.setXaPhuong(khachHangRequest.getXaPhuong());
+            khachHang.setQuanHuyen(khachHangRequest.getQuanHuyen());
+            khachHang.setTinhThanhPho(khachHangRequest.getTinhThanhPho());
+            khachHang.setTrangThai(khachHangRequest.getTrangThai());
+            java.util.Date currentDate = new java.util.Date();
+            khachHang.setNgaySua(new java.sql.Date(currentDate.getTime()));
+
+            khachHangRepository.save(khachHang);
+            System.out.println("KhachHangService.update: " + khachHang.getHoVaTen());
+        } else {
+            System.out.println("KhachHangService.update: null");
+        }
+    }
+
+    public void delete(UUID id) {
+        KhachHang khachHang = khachHangRepository.findById(id).orElse(null);
+        if (khachHang != null) {
+            khachHangRepository.deleteById(id);
+
+            System.out.println("KhachHangService.delete: " + id);
+        } else {
+            System.out.println("KhachHangService.delete: null");
+        }
+    }
+
+    public KhachHangResponse getById(UUID id) {
+        KhachHangResponse khachHangResponse = khachHangRepository.getByIdResponse(id);
+        if (khachHangResponse != null) {
+            System.out.println("KhachHangService.findById: " + khachHangResponse.getHoVaTen());
+            return khachHangResponse;
+        } else {
+            System.out.println("KhachHangService.findById: null");
+            return null;
+        }
+    }
+    public KhachHang getById1(UUID id) {
+        KhachHang khachHang = khachHangRepository.getById(id);
+        if (khachHang != null) {
+            System.out.println("KhachHangService.findById: " + khachHang.getHoVaTen());
+            return khachHang;
+        } else {
+            System.out.println("KhachHangService.findById: null");
+            return null;
+        }
+    }
